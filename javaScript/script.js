cc1b03a8085432d6abce8f7aa8c55fb2925a0bc6
sunny chahal <sunnychaha555@gmail.com>
2026-03-20 08:16:06 +0530

    JAVA SCRIPT

---
+++ javaScript/script.js
@@ -0,0 +1,12 @@
+// let months = ["january", "july", "march", "august"];
+// months.splice(0, 1);
+// months.splice(1, 0,  "june");
+// console.log(months);
+
+
+
+let lan = ["c", "java", "ruby", "javascript", "python", "react"];
+
+
+
+console.log(lan.reverse().indexOf("javascript"));
