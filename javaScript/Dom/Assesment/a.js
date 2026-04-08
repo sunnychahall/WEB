3b678dcfb3da5cefc12077aa6c1304c7e2d716ee
sunny chahal <sunnychaha555@gmail.com>
2026-04-08 19:59:11 +0530

    assesment

---
+++ javaScript/Dom/Assesment/a.js
@@ -0,0 +1,15 @@
+let btn = document.createElement('button');
+btn.innerText = "Click Me!";
+document.querySelector('body').append(btn);
+
+btn.setAttribute('id', 'btn');
+btn.setAttribute('placeholder', 'username');
+
+
+btn.querySelector('#btn');
+btn.classList.add('styleBlue');
+
+
+let para = document.createElement('p');
+para.innerHTML = "Sunny <b>Chahal</b>";
+document.querySelector('body').append(para);
